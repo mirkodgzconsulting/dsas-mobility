1c0e5b7dd89665d5871d36e801db178a808f8097
DSAS Mobility Dev <dev@dsas-mobility.it>
2026-01-14 12:40:32 +0100

    chore: fix gitignore and remove tracked build artifacts

--- .vercel/output/_functions/chunks/supabase_84VYV0jg.mjs
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://qudtubjjqhibwwwwafwb.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1ZHR1YmpqcWhpYnd3d3dhZndiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzMzAxMTYsImV4cCI6MjA4MzkwNjExNn0.FDDMHAoxMsfyCh7-ir5OKPllw_tJ5OJhGkzqV5b0yzs";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export { supabase as s };
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_Bvblyy7r.mjs';
-export { renderers } from '../renderers.mjs';
